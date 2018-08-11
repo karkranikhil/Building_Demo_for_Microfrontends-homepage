7c5ea8a227c77333b39ec825cc8b3ba7cc5b62cf
karkran <nikhil.karkra@anz.com>
2018-08-12 02:03:59 +1000

    adding web component

--- server.js
+++ server.js
@@ -1,29 +1,53 @@
+// const express = require('express');
+// const server = express();
+// const request = require('request');
+// const proxy = require('http-proxy-middleware');
+//
+// server.set('view engine', 'ejs');
+//
+// const createProxy = (path, target) =>server.use(path, proxy({ target, changeOrigin: true, pathRewrite: {[`^${path}`]: ''} }));
+//
+// createProxy('/header', 'https://microfrontends-component-one.herokuapp.com/');
+// createProxy('/products', 'https://microfrontends-component-two.herokuapp.com/');
+// createProxy('/cart', 'https://microfrontends-component-three.herokuapp.com/');
+//
+// server.get('/', (req, res) => res.render('index'));
+//
+// const port = process.env.PORT || 8082;
+// server.listen(port, () => {
+//   console.log(`Homepage listening on port ${port}`);
+// });
+
+// const express = require('express');
+// const server = express();
+// const request = require('request');
+//
+// server.set('view engine', 'ejs');
+//
+// server.get('/', (req, res) => res.render('index'));
+//
+// const port = process.env.PORT || 8081;
+// server.listen(port, () => {
+//   console.log(`Homepage listening on port ${port}`);
+// });
+
 const express = require('express');
 const server = express();
 const request = require('request');
+const proxy = require('http-proxy-middleware');
 
 server.set('view engine', 'ejs');
 
-server.get('/', (req, res) => Promise.all([
-  getContents('https://microfrontends-component-one.herokuapp.com/'),
-  getContents('https://microfrontends-component-two.herokuapp.com/'),
-  getContents('https://microfrontends-component-three.herokuapp.com/')
-]).then(responses =>
-res.render('index', { header: responses[0], productsList: responses[1], cart: responses[2] })
-).catch(error =>
-res.send(error.message)
-)
-);
+const createProxy = (path, target) =>
+server.use(path, proxy({ target, changeOrigin: true, ignorePath: true }));
 
-const getContents = (url) => new Promise((resolve, reject) => {
-  request.get(url, (error, response, body) => {
-  if (error) return resolve("Error loading " + url + ": " + error.message);
+createProxy('/header', 'https://microfrontends-component-one.herokuapp.com/');
+createProxy('/products-list', 'https://microfrontends-component-two.herokuapp.com/');
+createProxy('/cart', 'https://microfrontends-component-three.herokuapp.com/');
 
-return resolve(body);
-});
-});
+server.get('/', (req, res) => res.render('index'));
 
-const port = process.env.PORT || 8088;
+const port = process.env.PORT || 8085;
 server.listen(port, () => {
   console.log(`Homepage listening on port ${port}`);
 });
